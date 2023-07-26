2308dbb12ab86da2ddb11d30e17d2335d68682de
Ömer EROĞLU <omereroglu1923@gmail.com>
2023-07-26 10:12:51 +0300

    Engin Demirog JavaScript 1. Gün Kamp Sonu

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
 let dolarBugun = 9.30
-
 let dolarDun = 9.20
-dolarDun = "9.20"
+
 {
     let dolarDun = 9.10
 }
-
 console.log(dolarDun)
 
 const euroDun = 11.2
@@ -17,20 +14,10 @@ const euroDun = 11.2
 console.log(euroDun)
 
 //array
-//camelCasing
-//PascalCasing
 let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")
 }
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
